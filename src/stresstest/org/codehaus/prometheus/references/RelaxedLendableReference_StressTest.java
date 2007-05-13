e1d082ebade4f45e4e2ddcf89eeab1d73af84aae
alarmnummer <alarmnummer@8a1064f6-bd2c-0410-98eb-edd5a03a0880>
2007-05-13 06:58:47 +0000

    -added body for stresstest

---
+++ src/stresstest/org/codehaus/prometheus/references/RelaxedLendableReference_StressTest.java
@@ -0,0 +1,11 @@
+package org.codehaus.prometheus.references;
+
+import junit.framework.TestSuite;
+
+public class RelaxedLendableReference_StressTest {
+
+    public static TestSuite suite(){
+        TestSuite suite = new TestSuite();
+        return suite;
+    }
+}
