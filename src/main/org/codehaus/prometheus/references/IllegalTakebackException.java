cb45bb5bdcd25ad405639e80d2d315d142506b8f
alarmnummer <alarmnummer@8a1064f6-bd2c-0410-98eb-edd5a03a0880>
2007-05-13 06:15:28 +0000

    - improved unittests - joined lendablereference/awaitablereference in same package

---
+++ src/main/org/codehaus/prometheus/references/IllegalTakebackException.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2007 Peter Veentjer
+ *
+ * This program is made available under the terms of the MIT License.
+ */
+package org.codehaus.prometheus.references;
+
+/**
+ * A RuntimeException thrown when attempting to take back an invalid reference to a
+ * {@link org.codehaus.prometheus.references.LendableReference}.
+ *
+ * @author Peter Veentjer.
+ */
+public class IllegalTakebackException extends RuntimeException{
+
+    public IllegalTakebackException(){}
+
+    public IllegalTakebackException(String msg){
+        super(msg);
+    }
+}
